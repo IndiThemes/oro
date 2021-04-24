cfed8c4002cdb08ceed7564a15d4458a06c5574e
Divjot <divjotdavy@gmail.com>
2021-04-25 00:54:21 +0530

    test

--- search.php
+++ search.php
@@ -2,9 +2,9 @@
 /**
  * The template for displaying search results pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result  
  *
- * @package Oro
+ * @package Oro Theme
  */
 
 get_header(NULL, ['layout'	=>	'container', 'header' => 'search']);
